5bc9b6266931df465cf143b8d0458e44ad102c29
guilhermearaujoo <ghma.em1@gmail.com>
2022-09-27 00:30:19 -0300

    Comecando a fazer a conversao de numeros romanos

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-4.4/bonus/numerosRomanos.js
@@ -0,0 +1,47 @@
+// Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+
+// Atenção! Esse exercício já apareceu no processo seletivo de uma grande multinacional!
+
+// Dicas:
+
+// Uma string é um array de caracteres, então cada elemento do array é uma letra.
+// O valor de cada numeral romano é:
+
+let numRomanos = {
+  "I": 1,
+  "IV": 4,
+  'IX': 9,
+  'X': 10,
+  'XL': 40,
+  'L': 50,
+  'XC': 90,
+  'C': 100,
+  'CD': 400,
+  'D': 500,
+  'CM': 900,
+  'M': 1000, 
+}
+
+function decToRoman(converter){
+  let listConverter = converter.split('');
+  
+  // verifica se as letras são validas
+  for(let index of listConverter){
+    let isDecimal = false;
+    for(let key in numRomanos){
+      if(key == index){
+        isDecimal = true;
+      }
+    }
+    //retorna erro se for um numero invalido
+    if(!isDecimal){
+      return "ERROR: Must be a valid a ROMAN NUMBER";
+    }
+  }
+
+
+  return true;
+  
+}
+
+console.log(decToRoman('XMI'));
