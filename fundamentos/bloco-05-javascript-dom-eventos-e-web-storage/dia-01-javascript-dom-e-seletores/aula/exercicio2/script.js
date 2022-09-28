786146fb8282d85e2925ba5d65c0df7758746756
guilhermearaujoo <ghma.em1@gmail.com>
2022-09-28 18:11:27 -0300

    feito segundo exercicio da aula

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/aula/exercicio2/script.js
@@ -0,0 +1,6 @@
+let paragraphs = document.getElementsByClassName('sameParagraph');
+
+paragraphs[0].style.color = 'red';
+
+let subtitle = document.getElementsByTagName('h4');
+subtitle[0].style.backgroundColor = 'yellow';
