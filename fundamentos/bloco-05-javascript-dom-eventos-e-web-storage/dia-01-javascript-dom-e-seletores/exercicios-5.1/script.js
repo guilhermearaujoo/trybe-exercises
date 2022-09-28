05d3e7fd689ff7b15d44e7294d5ed1cefdb81f48
guilhermearaujoo <ghma.em1@gmail.com>
2022-09-28 19:00:43 -0300

    Feitos requisitos 2 e 3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicios-5.1/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicios-5.1/script.js
@@ -1,3 +1,7 @@
 let twoYears = document.getElementsByTagName('p')[1];
 
 twoYears.innerHTML = 'Two years from now I see myself working in a multinacional enterprise, winning more than 4 thousand dollars month and having more than 1 project assigned in my name.'
+
+document.querySelector('.main-content').style.backgroundColor = 'rgb(76,164,109)';
+
+document.querySelector('.main-content .center-content').style.backgroundColor = 'white';
