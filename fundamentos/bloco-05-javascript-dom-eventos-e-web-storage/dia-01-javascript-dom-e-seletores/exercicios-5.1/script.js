10a35093491c509da293aaf75bcbedb5638aec08
guilhermearaujoo <ghma.em1@gmail.com>
2022-09-28 19:16:34 -0300

    Feito ultimo requisitos e terminado

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicios-5.1/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicios-5.1/script.js
@@ -5,3 +5,23 @@ twoYears.innerHTML = 'Two years from now I see myself working in a multinacional
 document.querySelector('.main-content').style.backgroundColor = 'rgb(76,164,109)';
 
 document.querySelector('.main-content .center-content').style.backgroundColor = 'white';
+
+document.getElementsByTagName('h1')[0].innerHTML = 'Exercício - JavaScript';
+
+
+function paragraphUpperCase() {
+  let paragraph = document.getElementsByTagName('p')[0];
+
+  paragraph.innerHTML = paragraph.innerHTML.toUpperCase()
+}
+
+paragraphUpperCase();
+
+function allClasses() {
+  let allP = document.getElementsByTagName('p');
+  for (let index = 0; index < allP.length; index++) {
+    console.log(allP[index].innerHTML);
+  }
+}
+
+allClasses(); 
