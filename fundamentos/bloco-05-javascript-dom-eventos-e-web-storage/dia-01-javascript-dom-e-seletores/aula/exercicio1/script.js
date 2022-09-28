dfa54ae67a8e713cf2777577df2798462a61837b
guilhermearaujoo <ghma.em1@gmail.com>
2022-09-28 15:56:23 -0300

    Feito primeiro exercio da aula

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/aula/exercicio1/script.js
@@ -0,0 +1,17 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const title = document.getElementById("page-title");
+title.innerText = "Aprendendo DOM com maestria";
+
+const secondParagraph = document.getElementById("second-paragraph");
+
+secondParagraph.style.backgroundColor = "black";
+
+secondParagraph.style.color = "green";
+
+const subtitle = document.getElementById("subtitle");
+
+subtitle.innerText = "This is my first DOM subtitle"
+
+subtitle.style.fontSize = "30px";
