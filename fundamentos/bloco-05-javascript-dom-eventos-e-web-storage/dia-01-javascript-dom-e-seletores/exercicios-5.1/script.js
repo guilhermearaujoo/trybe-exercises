96d3daf5da2c5514ad1f702bcc8e6119dde5ef73
guilhermearaujoo <ghma.em1@gmail.com>
2022-09-28 18:41:06 -0300

    Feito requisito 1

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicios-5.1/script.js
@@ -0,0 +1,3 @@
+let twoYears = document.getElementsByTagName('p')[1];
+
+twoYears.innerHTML = 'Two years from now I see myself working in a multinacional enterprise, winning more than 4 thousand dollars month and having more than 1 project assigned in my name.'
